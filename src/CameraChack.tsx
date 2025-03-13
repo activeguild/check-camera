8767f175ed2767cc8e280259b702926dfd0a027a
j1ngzoue <workdrive0306@gmail.com>
2025-03-13 17:48:08 +0900

    chore

--- src/CameraChack.tsx
+++ src/CameraChack.tsx
@@ -8,10 +8,13 @@ function CameraCheck() {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
       console.log("カメラは使用可能");
       setStatus("カメラは使用可能");
-      
+
       // 取得したストリームを即時停止
-      stream.getTracks().forEach(track => track.stop());
+      stream.getTracks().forEach((track) => track.stop());
     } catch (error) {
+      if (error instanceof Error === false) {
+        return;
+      }
       if (error.name === "NotReadableError") {
         console.log("カメラが他のアプリまたはタブで使用中の可能性があります");
         setStatus("カメラが他のアプリで使用中の可能性あり");
